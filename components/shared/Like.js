c4409683c166257c6313349a20a7ee1194cfad9c
sayamjn <sayamjain0708@gmail.com>
2023-09-09 17:05:56 +0530

    like count

--- components/shared/Like.js
+++ components/shared/Like.js
@@ -1,5 +1,5 @@
 "use client"
-import { likeThread, unlikeThread } from "../../lib/actions/thread.actions";
+import { likeThread, unlikeThread, getLikesCount } from "../../lib/actions/thread.actions"; // You might need to add a function to get the likes count
 import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import { usePathname } from "next/navigation";
@@ -7,21 +7,28 @@ import { useState, useEffect } from "react";
 
 export default function Like({
   post, // Simple value
-  likes = [], // Likes array (defaulted to empty array)
 }) {
   const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0); // Initialize likes count to 0
 
   const { user } = useUser();
   const pathname = usePathname();
 
-  // Load the liked state from local storage on component mount
+  // Load the liked state and likes count from local storage on component mount
   useEffect(() => {
-    const localStorageKey = `liked-${post}`;
-    const storedLiked = localStorage.getItem(localStorageKey);
-    if (storedLiked) {
-      setLiked(storedLiked === "true");
+    if (user) {
+      const localStorageKey = `liked-${post}-${user.id}`;
+      const storedLiked = localStorage.getItem(localStorageKey);
+      if (storedLiked) {
+        setLiked(storedLiked === "true");
+      }
+
+      // Retrieve the likes count for the post and update the state
+      getLikesCount(post).then((count) => {
+        setLikesCount(count);
+      });
     }
-  }, [post]);
+  }, [post, user]);
 
   const handleLike = () => {
     if (!user) {
@@ -32,8 +39,9 @@ export default function Like({
       likeThread(post, user.id, pathname)
         .then(() => {
           setLiked(true);
-          // Store the liked state in local storage
-          const localStorageKey = `liked-${post}`;
+          // Update the likes count and store the liked state in local storage
+          setLikesCount(likesCount + 1);
+          const localStorageKey = `liked-${post}-${user.id}`;
           localStorage.setItem(localStorageKey, "true");
         })
         .catch((error) => console.error("Failed to like thread:", error));
@@ -41,8 +49,9 @@ export default function Like({
       unlikeThread(post, user.id, pathname)
         .then(() => {
           setLiked(false);
-          // Remove the liked state from local storage
-          const localStorageKey = `liked-${post}`;
+          // Update the likes count and remove the liked state from local storage
+          setLikesCount(likesCount - 1);
+          const localStorageKey = `liked-${post}-${user.id}`;
           localStorage.removeItem(localStorageKey);
         })
         .catch((error) => console.error("Failed to unlike thread:", error));
@@ -50,14 +59,17 @@ export default function Like({
   };
 
   return (
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        handleLike();
-      }}
-    >
-      <Heart fill={liked ? "#dc2626" : "#0a0a0a"} className="w-5 h-5" />
-    </button>
+    <div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          handleLike();
+        }}
+      >
+        <Heart fill={liked ? "#dc2626" : "#0a0a0a"} className="w-5 h-5" />
+      </button>
+      <span className="text-small-regular text-light-2">{likesCount} Likes</span>
+    </div>
   );
 }
